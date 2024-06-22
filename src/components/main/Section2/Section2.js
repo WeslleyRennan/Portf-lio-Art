cdd51b7e2cfbdd0f8648fce0bb79d7a78eb3b8cc
WeslleyRennan <weslleyrennan535@gmail.com>
2024-06-22 11:36:37 -0300

    Correções

--- src/components/main/Section2/Section2.js
+++ src/components/main/Section2/Section2.js
@@ -1,9 +1,17 @@
 import style from "./Section2.module.css"
+import Referencia1 from "./Referencia1.jpeg"
+import Referencia2 from "./Referencia2.jpeg"
+import Referencia3 from "./Referencia3.jpeg"
 
 function Section2 (){
     return(
         <section className={style.BoxEsboco}>
-            <div></div>
+            <h1 className={style.TextProcesso}>Processo</h1>
+            <div className={style.BoxEsbocoImg}>
+                <img src={Referencia1} className={style.EsbocoImg}/>
+                <img src={Referencia2} className={style.EsbocoImg}/>
+                <img src={Referencia3} className={style.EsbocoImg}/>
+            </div>
         </section>
     )
 }
