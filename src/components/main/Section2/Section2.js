4912c52f161d903c2ec24f85dcb8519f206d0d28
WeslleyRennan <weslleyrennan535@gmail.com>
2024-06-27 10:47:37 -0300

    Commit4

--- src/components/main/Section2/Section2.js
+++ src/components/main/Section2/Section2.js
@@ -1,16 +1,16 @@
 import style from "./Section2.module.css"
-import Referencia1 from "./Referencia1.jpeg"
-import Referencia2 from "./Referencia2.jpeg"
-import Referencia3 from "./Referencia3.jpeg"
+import Desenho1 from "./Desenho1.jpg"
+import Desenho2 from "./Desenho2.jpg"
+import Desenho3 from "./Desenho3.jpg"
 
 function Section2 (){
     return(
         <section className={style.BoxEsboco}>
             <h1 className={style.TextProcesso}>Processo</h1>
             <div className={style.BoxEsbocoImg}>
-                <img src={Referencia1} className={style.EsbocoImg}/>
-                <img src={Referencia2} className={style.EsbocoImg}/>
-                <img src={Referencia3} className={style.EsbocoImg}/>
+                <img src={Desenho1} className={style.EsbocoImg}/>
+                <img src={Desenho2} className={style.EsbocoImg}/>
+                <img src={Desenho3} className={style.EsbocoImg}/>
             </div>
         </section>
     )
